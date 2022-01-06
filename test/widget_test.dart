37d88954b09d57457c79522c0d3d6df4e64ab001
rafaelsantosferraz <rafaelsantosferraz@gmail.com>
2022-01-05 22:42:01 -0300

    Add test fetch series on start

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,99 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-import 'package:rafael_jobsity_challenge/main.dart';
+import 'widget_test.mocks.dart';
 
+class Movie{
+  final String title;
+  Movie(this.title);
+}
+
+typedef Series = Stream<List<Movie>>;
+class MovieLibrary{
+
+  final MovieRepository _movieRepository;
+  MovieLibrary(this._movieRepository);
+
+  final StreamController<List<Movie>> _seriesStream = StreamController();
+
+
+  Series get series => _seriesStream.stream;
+
+  addEvent(MovieLibraryEvent event){
+    _onEvent(event);
+  }
+
+  //region Private -------------------------------------------------------------
+  _onEvent(MovieLibraryEvent event){
+    switch(event.runtimeType){
+      case MovieLibraryStartEvent: _onStart(event as MovieLibraryStartEvent);
+      break;
+    }
+  }
+
+  void _onStart(MovieLibraryStartEvent event) async {
+    var series = await _movieRepository.getSeries();
+    _seriesStream.sink.add(series);
+  }
+  //endRegion
+
+
+  //region Public --------------------------------------------------------------
+  close(){
+    _seriesStream.close();
+  }
+  //endregion
+}
+
+
+
+class MovieLibraryEvent{
+
+  const MovieLibraryEvent._();
+
+  factory MovieLibraryEvent.start() = MovieLibraryStartEvent;
+}
+
+class MovieLibraryStartEvent extends MovieLibraryEvent {
+  const MovieLibraryStartEvent() : super._();
+}
+
+class MovieRepository {
+
+  Future<List<Movie>> getSeries() async{
+    return [];
+  }
+}
+
+
+@GenerateMocks([MovieRepository])
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  group('WHEN app start, app SHOULD list all of the series contained in the API used by the paging scheme provided by the API.', (){
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    final movieRepository = MockMovieRepository();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    test('On movie library start EVENT, movie library SHOULD fetch from movie repository and stream result ', () async {
+      // Given/Arrange
+      var movieLibrary = MovieLibrary(movieRepository);
+      var mockDbSeries = [
+        Movie('1'),
+        Movie('2'),
+        Movie('3'),
+        Movie('4'),
+        Movie('5'),
+      ];
+      when(movieRepository.getSeries()).thenAnswer((_) async => mockDbSeries);
+
+      // When/Act
+      movieLibrary.addEvent(MovieLibraryEvent.start());
+
+      // Then/Assert
+      expectLater(movieLibrary.series, emits(mockDbSeries));
+    });
   });
+
 }
